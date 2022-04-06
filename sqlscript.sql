afb4bd4a2abea46fb5c7933f2a27c5593dfa83ca
BenjaminOmar <Benjaminjoof01@gmail.com>
2022-04-07 01:38:26 +0200

    Oppdatert SQL
    Oppdatert SQL skriptet slik at man kan registrere flere personer på applikasjonen. Skal senere implementere innloggingssystem

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,18 +1,40 @@
+DROP TABLE IF EXISTS studielån;
 DROP TABLE IF EXISTS inntekt;
+DROP TABLE IF EXISTS bruker;
+
+CREATE TABLE bruker (
+    brukernavn varchar(40) NOT NULL UNIQUE,
+    Fornavn varchar(40),
+    Etternavn varchar(40),
+    PRIMARY KEY(brukernavn)
+    );
 
 CREATE TABLE inntekt (
-    år integer NOT NULL,
-    måned varchar(45) NOT NULL,
-    nettoinntekt integer,
-    husleie integer,
-    klær integer,
-    frisør integer,
-    buss integer,
-    sparing integer,
-    mat integer,
-    fylla integer,
-    tilOvers integer,
-    PRIMARY KEY (år, måned)
-);
+    År SMALLINT NOT NULL,
+    Måned varchar(40) NOT NULL,
+    brukernavn varchar(40),
+    Nettoinntekt integer,
+    Husleie integer,
+    Klær integer,
+    Frisør integer,
+    Buss integer,
+    Sparing integer,
+    Mat integer,
+    Fylla integer,
+    TilOvers integer,
+    PRIMARY KEY(År,Måned, brukernavn),
+    FOREIGN KEY(brukernavn) REFERENCES bruker(brukernavn)
+    );
+    
+CREATE TABLE studielån (
+    År SMALLINT,
+    Måned varchar(40),
+    brukernavn varchar(40),
+    Lån integer,
+    Stipend integer,
+    PRIMARY KEY (År, Måned, brukernavn),
+    FOREIGN KEY(År, Måned) REFERENCES inntekt(År, Måned),
+    FOREIGN KEY(brukernavn) REFERENCES bruker(brukernavn)
+    );
 
 ---------------------------------------
