e2a0881bb91305d424ab2923f5c8c4bf74a6a42c
BenjaminOmar <Benjaminjoof01@gmail.com>
2022-06-07 03:34:46 +0200

    Jobbet med redesign av økonomi hovedsiden
    Laget to klikkbokser som fører til registrering og visning av inntekt.

--- økonomi.php
+++ økonomi.php
@@ -1,13 +1,38 @@
 <?php
 include("header.php");
 ?>
-    <section class="main-content">
-        <div class="startside">
-            <h3>Velkommen til startsiden</h3>
-            <h5>Bruk menyen til venstre for å navigere</h5>
-
+<section class="main-content">
+    <div class="startside">
+        <div class="m-container">
+            <div class="m-registrer" onclick="location.href='reg-inntekt.php';">
+                <div>
+                    <div>
+                        <h3>Registrering av inntekt</h3>
+                    </div>
+                    <div>
+                        <p class="m-registrer-text">
+                            Her registrerer du inntekten din
+                        </p>
+                    </div>
+                    <div>
+                        <img src="bilder/registrering-icon.svg" class="m-registrer-bilde" alt="Icon av registrering">
+                    </div>
+                </div>
+            </div>
+            <div class="m-vis" onclick="location.href='vis-inntekt.php';">
+                <div>
+                    <h3>Vis din inntekt</h3>
+                </div>
+                <div>
+                    <p class="m-vis-text">Her vises inntekten din</p>
+                </div>
+                <div>
+                    <img src="bilder/vis-inntekt-ikon.svg" class="m-vis-bilde" alt="Icon av visning">
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 </body>
 </html>
 <?php
