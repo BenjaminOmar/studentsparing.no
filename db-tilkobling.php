cda61f5b556e1a973d8beef62da8437979fb4b5e
BenjaminOmar <Benjaminjoof01@gmail.com>
2022-06-27 01:33:02 +0200

    Endringer på server og fonts
    - Endret på hvor backend ligger, fra database 1 servern, til min localhost.
    - Endret på font for overskrifter, slik at den matcher brødtekst
    - Tatt vekk den andre css filen, slik at det bare er en slik fil. Skal fikse på brukergrensesnittet til index filen etterhvert.

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,10 +1,10 @@
 <?php
 /* ----- Programmet kobler seg til databasen ----- */
 
-$host="web01.usn.no";
-$user="247061";
-$password="WO1SWwlt";
-$database="247061";
+$host="localhost";
+$user="root";
+$password="";
+$database="okonomisystem";
 
 $db=mysqli_connect($host, $user, $password, $database) or die ("Tilkoblingen til databasen mislyktes");
 
