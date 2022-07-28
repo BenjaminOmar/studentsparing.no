aa278a61660f78c4146fb679f2def87676f1fb14
BenjaminOmar <Benjaminjoof01@gmail.com>
2022-07-28 18:23:51 +0200

    stylet login og registrering
    Stylet login og registrerings undersidene ferdig, slik at de ser profesjonelle ut.

--- innlogging.php
+++ innlogging.php
@@ -2,19 +2,37 @@
 /* trenger php tagger her for sjekk */
 ?>
 
-<body class="i-container-body">
-    <div class="i-container">
-    <form action="" id="innloggingSkjema" name="innloggingSkjema" method="post">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body class="i-container-body">
+        <div class="i-container">
+    <form action="" id="registrerBrukerSkjema" name="registrerBrukerSkjema" method="post">
         <div>
             <img src="bilder/logo/Studentsparing-01-scaled.jpg" class="i-logo" alt="Studentsparing logo">
         </div>
-            Brukernavn <input name="brukernavn" type="text" id="brukernavn"> <br />
-            Passord <input name="passord" type="password" id="passord"  >  <br />
-            <input type="submit" name="logginnKnapp" value="Fortsett">
-            <input type="reset" name="nullstill" id="nullstill" value="Nullstill"> <br />
-</form>
+        <div class="i-text-username">
+            Brukernavn
+        </div>
+            <input class="i-box-username" name="brukernavn" type="text" id="brukernavn" required> <br />
+        <div class="i-text-password">
+            Passord
+        </div>
+            <input class="i-box-password" name="passord" type="password" id="passord" required>  <br />
+        <div class="i-buttons">
+            <input class="i-button-p" type="submit" name="logginnKnapp" value="Fortsett">
+            <input class="i-button-r" type="reset" name="nullstill" id="nullstill" value="Nullstill"> <br />
+        </div>
+    </form>
 </div>
 </body>
+</html>
 
 <?php
   if (isset($_POST ["logginnKnapp"]))
